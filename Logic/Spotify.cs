d62d2baf20b7c283e8434791068b1ee4a1e1ed14
AlphaBlade01 <69124540+AlphaBlade01@users.noreply.github.com>
2024-10-25 21:58:55 +0100

    Custom builder + client authorisation using PKCE + basic methods

--- Logic/Spotify.cs
+++ Logic/Spotify.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
+using MultiplaylistAdder.Models;
 using Newtonsoft.Json;
 using SpotifyAPI.Web;
 using System.Diagnostics;
-using System.Net.Http.Json;
-using System.Text.RegularExpressions;
+using System.Net;
 
 namespace MultiplaylistAdder.Logic;
 
@@ -11,50 +11,160 @@ public class Spotify
 {
     private const string ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token?grant_type=client_credentials&client_id={0}&client_secret={1}";
     private const string ACCESS_TOKEN_PATTERN = @"/""access_token"":\s*""(\S*)""/gm";
+    private const string REDIRECT_BASE_URL = "http://localhost:3000/callback/";
 
-    private readonly JsonSerializer jsonSerializer = new();
-    private readonly HttpClient _httpClient = new();
-    private readonly SpotifyClient _client;
+    private static readonly JsonSerializer jsonSerializer = new();
+    private static readonly HttpClient _httpClient = new();
     private readonly IConfiguration _configuration;
     private readonly string _clientId;
     private readonly string _clientSecret;
-    private string _accessToken;
-    
-    public Spotify(IConfiguration configuration)
+    private SpotifyClient _client;
+    private IUserProfileClient _userProfile;
+
+
+    public Spotify(IConfiguration configuration, SpotifyClient client)
     {
         _configuration = configuration;
         _clientId = configuration["Spotify:ClientId"];
         _clientSecret = configuration["Spotify:ClientSecret"];
-        // Instantiate client
+        _client = client;
+        _userProfile = client.UserProfile;
     }
 
-    public void GetCurrentlyListening()
+    public static Spotify BuildClient(IConfiguration configuration)
     {
+        return Task.Run(async () =>
+        {
+            string? clientId = configuration["Spotify:ClientId"];
+            string? clientSecret = configuration["Spotify:ClientSecret"];
 
+            if (clientId is null || clientSecret is null) throw new Exception("ClientId or secret is null");
+
+            HttpListener loginCallbackServer = CreateLoginCallbackServer();
+            string codeVerifier = LoginUser(clientId);
+            string? code = await ListenOnLoginCallbackServer(loginCallbackServer);
+
+            if (code != null)
+            {
+                SpotifyClient client = await InitialiseClient(code, clientId, codeVerifier);
+                return new Spotify(configuration, client);
+            }
+
+            throw new Exception("Code received from spotify was null");
+        }).GetAwaiter().GetResult();
     }
 
-    private async Task<string?> GetAccessToken(string clientSecret)
+    public async Task<CurrentlyPlaying> GetCurrentlyListening()
     {
-        string url = String.Format(ACCESS_TOKEN_URL, _clientId, _clientSecret);
-        HttpContent content = JsonContent.Create(new Dictionary<string, string> { { "Content-Type", "application/x-www-form-urlencoded" } });
+        PlayerCurrentlyPlayingRequest request = new();
+        CurrentlyPlaying? currentlyListening = await _client.Player.GetCurrentlyPlaying(request);
+        return currentlyListening;
+    }
 
+    public async Task<List<Playlist>> GetPlaylists()
+    {
+        var playlists = await _client.PaginateAll(await _client.Playlists.CurrentUsers());
+        List<Playlist> result = [];
+
+        if (playlists is null) return result;
+
+        foreach (var playlist in playlists)
+        {
+            if (playlist is null || playlist.Owner?.Id != (await _client.UserProfile.Current()).Id) continue;
+
+            var newPlaylist = new Playlist
+            {
+                Name = playlist.Name ?? "",
+                Id = playlist.Id ?? "",
+                Selected = false
+            };
+            result.Add(newPlaylist);
+        }
+        return result;
+    }
+
+    public async Task<bool> AddSongToPlaylists(List<Playlist> playlists, FullTrack song)
+    {
         try
         {
-            HttpResponseMessage response = await _httpClient.PostAsync(url, content);
-
-            using Stream stream = await response.Content.ReadAsStreamAsync();
-            using StreamReader reader = new(stream);
-            string body = reader.ReadToEnd();
-
-            Regex pattern = new(ACCESS_TOKEN_PATTERN);
-            string token = pattern.Matches(body)[0].Value;
-
-            return token;
+            foreach (var playlist in playlists)
+            {
+                var request = new PlaylistAddItemsRequest([song.Uri]);
+                await _client.Playlists.AddItems(playlist.Id, request);
+            }
         }
         catch (Exception ex)
-        {   
-            Debug.WriteLine(ex);
-            return "";
+        {
+            Console.WriteLine(ex);
+            return false;
         }
+        return true;
+    }
+
+    private static async Task<SpotifyClient> InitialiseClient(string code, string clientId, string codeVerifier)
+    {
+        var tokenResponse = await new OAuthClient().RequestToken(
+            new PKCETokenRequest(clientId, code, new Uri(REDIRECT_BASE_URL), codeVerifier)
+        );
+        var authenticator = new PKCEAuthenticator(clientId, tokenResponse);
+        var config = SpotifyClientConfig.CreateDefault().WithAuthenticator(authenticator);
+
+        return new SpotifyClient(config);
+    }
+
+    private static HttpListener CreateLoginCallbackServer()
+    {
+        HttpListener listener = new();
+        listener.Prefixes.Add(REDIRECT_BASE_URL);
+        listener.Start();
+        return listener;
+    }
+
+    private static async Task<string?> ListenOnLoginCallbackServer(HttpListener listener)
+    {
+        HttpListenerContext context = await listener.GetContextAsync();
+        string? code = context.Request.QueryString["code"];
+        context.Response.StatusCode = code == null ? 400 : 200;
+        return code;
+    }
+
+    private static string LoginUser(string clientId)
+    {
+        string codeVerifier, codeChallenge;
+        (codeVerifier, codeChallenge) = PKCEUtil.GenerateCodes();
+
+        LoginRequest loginRequest = new(
+            new Uri(REDIRECT_BASE_URL),
+            clientId,
+            LoginRequest.ResponseType.Code
+        )
+        {
+            CodeChallengeMethod = "S256",
+            CodeChallenge = codeChallenge,
+            Scope = [
+                Scopes.PlaylistReadPrivate,
+                Scopes.PlaylistReadCollaborative,
+                Scopes.UserReadCurrentlyPlaying,
+                Scopes.PlaylistModifyPrivate,
+                Scopes.PlaylistModifyPublic
+            ]
+        };
+
+        var uri = loginRequest.ToUri();
+        try
+        {
+            var processStartInfo = new ProcessStartInfo()
+            {
+                FileName = uri.AbsoluteUri,
+                UseShellExecute = true
+            };
+            _ = Process.Start(processStartInfo);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return codeVerifier;
     }
 }
